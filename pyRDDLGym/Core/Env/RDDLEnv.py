713a27c3820ac44a4cc75f18a54a9617ec80755e
Bharath Shankar <coolbharath2@gmail.com>
2023-08-17 12:12:18 +0530

    Update RDDLEnv.py

--- pyRDDLGym/Core/Env/RDDLEnv.py
+++ pyRDDLGym/Core/Env/RDDLEnv.py
@@ -284,7 +284,7 @@ class RDDLEnv(gym.Env):
             self.sampler.check_action_preconditions(clipped_actions)
         
         # sample next state and reward
-        orig_subs = deepcopy(self.sampler.subs)
+        orig_subs = copy.deepcopy(self.sampler.subs)
         obs, reward, done = new_sampler.step(clipped_actions)
         state = new_sampler.states
             
