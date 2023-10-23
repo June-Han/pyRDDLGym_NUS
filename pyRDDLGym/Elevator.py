e4bb754a4bb36827c9720b25b1f921150e8edd3a
Bharath Shankar <coolbharath2@gmail.com>
2023-10-23 12:18:20 +0800

    Update Elevator.py

--- pyRDDLGym/Elevator.py
+++ pyRDDLGym/Elevator.py
@@ -58,7 +58,7 @@ class Elevator(gymnasium.Env):
 
     def reset(self, seed=None):
         state = self.base_env.reset(seed=seed)
-        return self.state2disc(state), None
+        return (self.state2disc(state), None)
 
     def render(self):
         self.base_env.render(to_display=False)
