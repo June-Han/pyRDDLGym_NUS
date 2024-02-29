f37ebf5fd309ea6420fb17bb28a1485d6683876a
Bharath Shankar <coolbharath2@gmail.com>
2024-02-29 18:27:34 +0530

    Update Elevator.py

--- pyRDDLGym/Elevator.py
+++ pyRDDLGym/Elevator.py
@@ -9,7 +9,7 @@ from pyRDDLGym.Visualizer.MovieGenerator import MovieGenerator
 from pathlib import Path
 
 class Elevator(gym.Env):
-    def __init__(self, is_render=False, render_path='temp_vis', instance=3):
+    def __init__(self, is_render=False, render_path='temp_vis', instance=4):
         '''
         Discrete version of the Elevator example. Please do not modify this
         '''
